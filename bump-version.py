81b79f13ad01e9a6e11d6e08ccab1b238a6a0021
Mirco Bianchini <57708659+sonomirco@users.noreply.github.com>
2022-09-30 22:02:33 +1000

    Update bump-version.py

--- bump-version.py
+++ bump-version.py
@@ -3,8 +3,35 @@ import re
 import argparse
 import json 
 
+# https://www.thecodeforest.io/post/2022-01-04-automate-github-actions/automate-github-actions/
+def read_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Create player id dataframe")
+    parser.add_argument("--modules", nargs="+", default=["Dyn.Module", "Gh.Module"], help="modules listed in an array")
+    args = parser.parse_args()
+    return args
+
 def bump_versions():
-    print(f"::set-output name=module_bumped::Hello")
+    args = read_args()
+    inputs =  args.modules
+    bumped_versions = {}
+
+    with open('versions.yaml', 'r') as file:
+        prime_service = yaml.safe_load(file)
+
+        for input in inputs:
+            split_path = input.split('.') 
+            version = prime_service[split_path[0]][split_path[1]]
+            print(version)
+            if 'alpha' in version:
+                old_num = re.search('alpha[.]*(\d+)', version)[1]
+                bump_num = int(old_num)+1
+                new_version = re.sub('alpha.*\d+', f'alpha.{bump_num}', version)
+                bumped_versions[input]=new_version
+            else:
+                new_version = version + '-alpha.0'
+                bumped_versions[input]=new_version
+    json_object = json.dumps(bumped_versions) 
+    print(f"::set-output name=module_bumped::{json_object}")
 
 if __name__ == "__main__":
     bump_versions()
